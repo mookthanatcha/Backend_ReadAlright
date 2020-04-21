c3de2b7e1c76b8b4bcd45854b5e6fee892077fb7
mookthanatcha <mookkyammy@gmail.com>
2020-04-21 17:35:45 +0700

    [UPDATE] update maybe youlike

--- app/model/Reading.js
+++ app/model/Reading.js
@@ -1,27 +1,27 @@
-var sql = require("./db.js")
+var sql = require("./db.js");
 
 var Reading = function (reading) {
-    this.title = reading.title,
-        this.content = reading.content,
-        this.image = reading.image,
-        this.category_id = reading.category_id,
-        this.isActive = reading.isActive
+  (this.title = reading.title),
+    (this.content = reading.content),
+    (this.image = reading.image),
+    (this.category_id = reading.category_id),
+    (this.isActive = reading.isActive);
 };
 
 Reading.getAllReading = function (result) {
-    sql.query("Select * from reading", function (err, res) {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-        } else {
-            console.log("Reading : ", res);
-            result(null, res);
-        } 
-    });
-};    
+  sql.query("Select * from reading", function (err, res) {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+    } else {
+      console.log("Reading : ", res);
+      result(null, res);
+    }
+  });
+};
 
-Reading.createReading = function(newRead, result) {
-  sql.query("INSERT INTO reading set ?", newRead, function(err, res) {
+Reading.createReading = function (newRead, result) {
+  sql.query("INSERT INTO reading set ?", newRead, function (err, res) {
     if (err) {
       console.log("error: ", err);
       result(err, null);
@@ -34,7 +34,7 @@ Reading.createReading = function(newRead, result) {
       result(null, res.insertId, res.message);
     }
   });
-};    
+};
 
 // Reading.getReadingByCategoryId = function (categoryId, result) {
 //     sql.query("Select * from reading where category_id = ?", categoryId, function (
@@ -50,43 +50,48 @@ Reading.createReading = function(newRead, result) {
 //     });
 // };
 
-Reading.getReadingByCategoryId = function(categoryId, result) {
-    sql.query("Select * from reading join Category on reading.category_id = Category.category_id where reading.category_id = ?", categoryId, function(
-      err,
-      res
-    ) {
+Reading.getReadingByCategoryId = function (categoryId, result) {
+  sql.query(
+    "Select * from reading join Category on reading.category_id = Category.category_id join Views on reading.reading_id = Views.reading_id where reading.category_id = ? AND Views.is_Active = '0'",
+    categoryId,
+    function (err, res) {
       if (err) {
         console.log("error: ", err);
         result(err, null);
       } else {
         result(null, res);
       }
-    });
-  };
+    }
+  );
+};
 
-  Reading.getReadingByReadingId = function(readingId, result) {
-    sql.query("Select * from reading join Category on reading.category_id = Category.category_id where reading.reading_id = ?", readingId, function(
-      err,
-      res
-    ) {
+Reading.getReadingByReadingId = function (readingId, result) {
+  sql.query(
+    "Select * from reading join Category on reading.category_id = Category.category_id where reading.reading_id = ?",
+    readingId,
+    function (err, res) {
       if (err) {
         console.log("error: ", err);
         result(err, null);
       } else {
         result(null, res);
       }
-    });
-  }
+    }
+  );
+};
 
-  Reading.getNewReading = function(result) {
-    sql.query("SELECT * FROM reading ORDER BY reading_id DESC LIMIT 5;", function(err,res) {
+Reading.getNewReading = function (result) {
+  sql.query(
+    "SELECT * FROM reading ORDER BY reading_id DESC LIMIT 5;",
+    function (err, res) {
       if (err) {
         console.log("error: ", err);
         result(err, null);
       } else {
         result(null, res);
       }
-    });
-  }   
- 
-module.exports = Reading;    
+    }
+  );
+};
+
+module.exports = Reading;
