07f0fe9aa9e5f2d8ea88b10c5af69a6483c71999
mookthanatcha <mookkyammy@gmail.com>
2020-09-14 22:00:42 +0700

    [EDIT] vocabbox

--- app/model/VocabBox.js
+++ app/model/VocabBox.js
@@ -34,7 +34,7 @@ VocabBox.getNewVocab = function (result) {
 
   VocabBox.getVocabBoxByCateId = function (CateId, result) {
     sql.query(
-      "Select * from VocabBox b join Category c on c.category_id = b.category_id where c.category_id = ?",
+      "Select b.*,c.categoryName from VocabBox b join Category c on c.category_id = b.category_id where c.category_id = ?",
       CateId,
       function (err, res) {
         if (err) {
